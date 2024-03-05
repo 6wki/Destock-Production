e9ad79936c54210714545a320c0d1218ce606117
shawki.chikhim <mhcc213@gmail.com>
2024-03-05 22:09:50 +0100

    first commit

--- src/app/loading.tsx
+++
@@ -1,6 +0,0 @@
-import LoadingAnimation from "@/components/loadingAnimation/LoadingAnimation";
-
-export default function Loading() {
-  // You can add any UI inside Loading, including a Skeleton.
-  return <LoadingAnimation />;
-}
